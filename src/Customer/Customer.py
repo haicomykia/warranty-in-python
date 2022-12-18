2570fe57e53a4192076601280f1365d030bce28d
undefinedSynthesis <undefinedsynthesis@gmail.com>
2022-12-18 20:01:36 +0900

    保証期間の判定ロジックを修正

--- src/Customer/Customer.py
+++ src/Customer/Customer.py
@@ -18,14 +18,14 @@ class Customer:
         self.cancel_date = None
         self.warranty = warranty
 
-    def get_end_of_warranty(self):
+    def get_end_of_warranty(self) -> date:
         """
         保証期間の終了日を返す
         :return: 保証期間の終了日
         """
         return self.warranty.get_warranty_period() + self.start_date
 
-    def has_subscribed(self, warranty: warranty):
+    def has_subscribed(self, warranty: warranty) -> bool:
         """
         保証期間内か？
         :param warranty: 保証enumのメンバ
@@ -34,8 +34,20 @@ class Customer:
         if self.warranty != warranty:
             return False
 
-        end_date = self.get_end_of_warranty() if self.cancel_date is None else self.cancel_date
-        if end_date >= date.today() >= self.start_date:
+        # 解約していない
+        if self.cancel_date is None:
+            if self.get_end_of_warranty() >= date.today() >= self.start_date:
+                return True
+            return False
+
+        # 解約
+        if self.cancel_date > date.today() >= self.start_date:
             return True
 
         return False
+
+    def cancel_warranty(self) -> None:
+        """
+        保証を解約
+        """
+        self.cancel_date = date.today()
--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from src.Warranty.WarrantyEnum import Warranties
 
 if __name__ == "__main__":
     taro = Customer("太郎", date.today(), Warranties.FIVE_YEARS)
+    # taro.cancel_warranty()
     for warranty in Warranties:
         if taro.has_subscribed(warranty):
             print(taro.get_end_of_warranty())
